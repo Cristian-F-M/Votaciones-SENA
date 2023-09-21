f2841de44496ba39ad01ff8edad8241e6541f109
Cristian Morales <pirulomorales20@gmail.com>
2023-09-20 20:15:14 -0500

    Elimine el inmport de HttpSession que no se estaba utilizando

--- src/java/controladores/ControladorAdministrador.java
+++ src/java/controladores/ControladorAdministrador.java
@@ -11,7 +11,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import modelos.Administrador;
 import modelos.Rol;
 import modelos.TipoDocumento;
